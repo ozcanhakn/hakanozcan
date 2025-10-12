c0eb614934e288971b3bb29f57677b34b41a7f7a
komutanlogar1 <70779306+komutanlogar1@users.noreply.github.com>
2025-10-12 07:02:44 +0300

    05: project section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,13 @@
 import AboutSection from "@/components/AboutSection";
 import HeroSection from "@/components/HeroSection";
+import Projects from "@/components/ProjectsSection";
 
 export default function Page() {
   return (
     <div>
       <HeroSection />
       <AboutSection />
+      <Projects />
     </div>
       
   )
